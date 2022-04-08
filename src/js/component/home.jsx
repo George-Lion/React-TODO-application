cc6571f780091b47a0701cd65b5cf2cd67fbe3b7
Jorge Leon <jorgeleonpicture@gmail.com>
2022-04-08 17:07:59 +0000

    todo list

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,24 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
+import TareaForm from "./tareaForma";
+import Tarea from "./tarea";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
-
-//create your first component
 const Home = () => {
+	const [listaTareas, setListaTareas] = useState([]);
+
+	const nuevaTarea = (tarea) => {
+		setListaTareas([tarea, ...listaTareas]);
+	};
+
+	const borrarTarea = (id) => {
+		const listaFiltrada = listaTareas.filter((e, index) => index !== id);
+		setListaTareas(listaFiltrada);
+	};
+
+	console.log(listaTareas);
+
 	return (
-		<div>
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="Home caja mx-auto rounded mt-4">
+			<div className=" caja2">
+				<TareaForm nuevaTarea={nuevaTarea} />
+				<div className="lista">
+					{listaTareas.map((e, index) => (
+						<Tarea
+							tarea={e}
+							key={index}
+							borrarTarea={borrarTarea}
+							id={index}
+						/>
+					))}
+				</div>
+				<div className="num">number of tasks {listaTareas.length}</div>
+			</div>
 		</div>
 	);
 };
---
+++ src/js/component/tareaForma.js
@@ -0,0 +1,40 @@
+import React, { useState } from "react";
+
+const TareaForm = (props) => {
+	const [inputText, setInputText] = useState("");
+	const [validacion, setValidacion] = useState(true);
+
+	const manejarFormulario = (e) => {
+		setInputText(e.target.value);
+	};
+
+	const submit = (event) => {
+		event.preventDefault();
+		if (inputText.trim() !== "") {
+			props.nuevaTarea(inputText);
+			setInputText("");
+			setValidacion(true);
+		} else {
+			setValidacion(false);
+		}
+	};
+
+	return (
+		<div className="">
+			<div>
+				<h1 className="title mx-auto">TODO LIST</h1>
+			</div>
+			<form className="" onSubmit={submit}>
+				<span>Add task</span>
+				<input value={inputText} onChange={manejarFormulario} />
+				<button className="btn-grad">Add</button>
+				<div className="validacion"></div>
+			</form>
+			{!validacion && (
+				<div className="validacion">please write a task.</div>
+			)}
+		</div>
+	);
+};
+
+export default TareaForm;
